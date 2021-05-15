d1a0272a8b42a74ec48a74adf0c00da62f8aba0b
aklezovics <arturs.klezovics@raisin.com>
2021-05-15 15:15:10 +0200

    Initial commit

---
+++ src/main/java/com/klezovich/kafkacoaching/KafkaCoachingApplication.java
@@ -0,0 +1,13 @@
+package com.klezovich.kafkacoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCoachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaCoachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klezovich/kafkacoaching/KafkaCoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klezovich.kafkacoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCoachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
